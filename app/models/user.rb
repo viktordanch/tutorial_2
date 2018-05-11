83ed3f036f6f2dcd642050ef5d7a40ef81d6d9da
Vadim <gam.vadik@gmail.com>
2018-05-11 14:21:09 +0300

    temp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
-  has_many :microposts
-  validates :name, presence: true    # Replace FILL_IN with the right code.
-  validates :email, presence: true    # Replace FILL_IN with the right code.
+  #has_many :microposts
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  before_save { self.email = email.downcase }
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426182626) do
+ActiveRecord::Schema.define(version: 20180511102218) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
