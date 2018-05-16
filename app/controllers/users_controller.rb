1e23ff2ff2101e9496f79149f884dfae02cddd46
Vadim <gam.vadik@gmail.com>
2018-05-16 09:20:09 +0300

    7_signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  def new
+  def new         #render signup form
     @user = User.new
   end
 
@@ -8,4 +8,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     #debugger
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #succesfull
+    else
+      render 'new' #render view 'new.html.erb'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
