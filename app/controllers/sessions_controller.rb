b27668113bb575376115e79bf742d6b8598df741
Vadim <gam.vadik@gmail.com>
2018-05-17 11:22:06 +0300

    chapter8_add_currentUser

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user                      #eq     'redirect_to user_url(user)'
     else
-      # Create an error message.
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
   end
