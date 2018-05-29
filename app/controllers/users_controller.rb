3cb59df8925b66d7dd4ffd412cab9d9f2d75d179
Vadim <gam.vadik@gmail.com>
2018-05-29 10:47:45 +0300

    Activation test and refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = 'Please check your email to activate your account.'
       redirect_to root_url
     else
-      render 'new' # render view 'new.html.erb'
+      render 'new'
     end
   end
 
