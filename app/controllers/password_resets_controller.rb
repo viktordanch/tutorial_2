5df84ae01f7cbb8e6a8b5b24a4e904cdd8f8c32c
Vadim <gam.vadik@gmail.com>
2018-05-31 15:13:43 +0300

    chapter12

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,9 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,         only: [:edit, :update]
+  before_action :valid_user,       only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update] # expired password reset
 
-  def new
-  end
+  def new; end
 
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
@@ -16,6 +18,46 @@ class PasswordResetsController < ApplicationController
     end
   end
 
-  def edit
+  def edit; end
+
+  def update
+    if params[:user][:password].empty?                  # to an empty password and confirmation
+      @user.errors.add(:password, 'can\'t be empty')
+      render 'edit'
+    elsif @user.update_attributes(user_params)          # successful update
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = 'Password has been reset.'
+      redirect_to @user
+    else
+      render 'edit'                                     # failed update due to an invalid password
+    end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    # Checks expiration of reset token.
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = 'Password reset has expired.'
+        redirect_to new_password_reset_url
+      end
+    end
+
+    # Before filters
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Confirms a valid user.
+    def valid_user
+      unless (@user && @user.activated? &&
+          @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,27 +50,27 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location # puts URL in the session variable under the key :forwarding_url only for a GET request
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-  end
 
-  # Confirms the correct user.
-  def correct_user
-    @user = User.find(params[:id]) # retrieved user on id from url
-    redirect_to(root_url) unless current_user?(@user)
-  end
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location # puts URL in the session variable under the key :forwarding_url only for a GET request
+        flash[:danger] = 'Please log in.'
+        redirect_to login_url
+      end
+    end
 
-  # Confirms an admin user.
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id]) # retrieved user on id from url
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
