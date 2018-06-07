3497719a4aec43947c484fe714a5d987876595cd
Vadim <gam.vadik@gmail.com>
2018-06-07 11:20:54 +0300

    following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token, :reset_token
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name:  'Relationship',
-                                  foreign_key: 'follower_id',
+                                  foreign_key: 'follower_id', # user following another user
                                   dependent:   :destroy
 
+  # source parameter tells the source of the following array is the set of followed ids
+  has_many :following, through: :active_relationships, source: :followed
   has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save   :downcase_email
   before_create :create_activation_digest
 
@@ -80,6 +83,21 @@ class User < ApplicationRecord
   # See "Following users" for the full implementation.
   def feed
     Micropost.where('user_id = ?', id)
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
